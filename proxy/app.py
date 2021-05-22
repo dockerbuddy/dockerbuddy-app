42011bccf4410628e62f1d51691eb95f19395a41
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2021-05-22 16:15:45 +0200

    proxy set up

--- proxy/app.py
+++ proxy/app.py
@@ -1,14 +1,17 @@
-import influxdb_client
+from influxdb_client import InfluxDBClient
 from flask import Flask
 from flask import jsonify
 import os
 
 app = Flask(__name__)
 INFLUXDB_TOKEN = os.environ['INFLUXDB_TOKEN']
+client = InfluxDBClient(url="http://influxdb:8086", token=INFLUXDB_TOKEN)
+buckets_api = client.buckets_api()
 
 
 @app.route('/')
-def hello_world():
-
-    return jsonify({"INFLUXDB_TOKEN": INFLUXDB_TOKEN})
+def print_buckets():
+    buckets = buckets_api.find_buckets().buckets
+    print(buckets)
+    return jsonify({'INFLUXDB_TOKEN': INFLUXDB_TOKEN})
 
