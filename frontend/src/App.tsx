3aed6a7e4a8c39e94585a5e65a08705ecd5fe208
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2021-05-22 21:18:32 +0200

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Box, Container } from '@material-ui/core';
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <Box>Hello World!</Box>
+    </Container>
+  );
+};
+
+export default App;
