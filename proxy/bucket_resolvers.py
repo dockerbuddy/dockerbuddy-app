f5ff6909b33b37eb2159010dbbf07c7c7fc8f7ce
Jakub Solecki <jakub.solecki@onet.com.pl>
2021-05-28 21:13:01 +0200

    creating bucket WIP

---
+++ proxy/bucket_resolvers.py
@@ -0,0 +1,40 @@
+from utils import INFLUXDB_TOKEN, ENCODING
+import requests
+import json
+
+
+# retrieves (by default) first 20 buckets for InfluxDB
+# https://docs.influxdata.com/influxdb/v2.0/api/#operation/GetBuckets
+def fetch_all_buckets():
+    response = requests.get(
+        url='http://influxdb:8086/api/v2/buckets',
+        headers={
+            'Authorization': f'Token {INFLUXDB_TOKEN}'
+        }
+    )
+    buckets_dict = json.loads(response.content.decode(ENCODING))
+    print(buckets_dict, flush=True)
+    return buckets_dict
+
+
+# creates bucket with given attributes. Returns bucket metadata from InfluxDB
+# https://docs.influxdata.com/influxdb/v2.0/api/#operation/PostBuckets
+def create_bucket(name, org_id, retention):
+    response = requests.post(
+        url='http://influxdb:8086/api/v2/buckets',
+        headers={
+            'Authorization': f'Token {INFLUXDB_TOKEN}'
+        },
+        json=json.dumps({
+            'name': name,
+            'orgID': org_id,
+            'retentionRules': [
+                {
+                    'everySeconds': retention
+                }
+            ]
+        })
+    )
+    bucket = json.loads(response.content.decode(ENCODING))
+    print(bucket, flush=True)
+    return bucket
