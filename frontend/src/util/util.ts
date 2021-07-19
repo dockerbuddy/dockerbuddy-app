13540a968e8fc2186270086f00af5e3a3f7ba652
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2021-07-19 21:50:31 +0200

    Dockerfile for prod

--- frontend/src/util/util.ts
+++ frontend/src/util/util.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export function capitalizeFirstLetter(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
