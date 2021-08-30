d64a945412f1b08605fecc2f987054e0437d9c7d
Krzysztof Nalepa <krzysztof.nalepa@appliscale.io>
2021-08-30 22:48:09 +0200

    Data model for metrics added

---
+++ backend/src/main/kotlin/pl/edu/agh/dockerbuddy/model/metric/ContainerSummary.kt
@@ -0,0 +1,11 @@
+package pl.edu.agh.dockerbuddy.model.metric
+
+data class ContainerSummary(
+        val id: String,
+        val name: String,
+        val image: String,
+        //TODO replace to enum after @Bartłomiej Plewnia will define them
+        val status: String,
+        val cpuUsage: BasicMetric,
+        val memoryUsage: BasicMetric
+)
