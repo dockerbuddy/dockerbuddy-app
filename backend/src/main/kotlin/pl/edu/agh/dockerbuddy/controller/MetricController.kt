c992a1570449a8b5be43d9df7ddeb087132adb68
Krzysztof Nalepa <krzysztof.nalepa@appliscale.io>
2021-08-30 23:27:02 +0200

    endpoint for posting metrics added

---
+++ backend/src/main/kotlin/pl/edu/agh/dockerbuddy/controller/MetricController.kt
@@ -0,0 +1,27 @@
+package pl.edu.agh.dockerbuddy.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import pl.edu.agh.dockerbuddy.controller.response.DefaultResponse
+import pl.edu.agh.dockerbuddy.controller.response.ResponseType
+import pl.edu.agh.dockerbuddy.model.metric.BasicMetric
+import pl.edu.agh.dockerbuddy.model.metric.HostSummary
+
+@RestController
+@RequestMapping("/api/v2/metrics")
+class MetricController {
+
+    @PostMapping
+    @RequestMapping("/{hostId}")
+    fun addMetric(@RequestBody hostSummary: HostSummary, @PathVariable hostId: String): ResponseEntity<DefaultResponse> {
+        return try {
+            //DO SOME STUFF HERE
+            ResponseEntity.status(HttpStatus.OK)
+                    .body(DefaultResponse(ResponseType.SUCCESS, "Metric uploaded", null))
+        } catch (e: Exception){
+            ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
+                    .body(DefaultResponse(ResponseType.ERROR, e.toString(), null))
+        }
+    }
+}
---
+++ backend/src/main/kotlin/pl/edu/agh/dockerbuddy/controller/response/ResponseType.kt
@@ -0,0 +1,6 @@
+package pl.edu.agh.dockerbuddy.controller.response
+
+enum class ResponseType {
+    ERROR,
+    SUCCESS
+}
