edf7ea9e4c7d6f306fbc6aef4b2d2453faa1b260
Krzysztof Nalepa <56135216+kraleppa@users.noreply.github.com>
2021-09-04 18:54:52 +0200

    Issue28 create cache module (#36)
    * Metric with alert class added
    
    * InMemory interface defined
    
    * In memory storage added
    
    * metric model established
    
    * Tests added
    
    * imports deleted

---
+++ backend/src/main/kotlin/pl/edu/agh/dockerbuddy/model/AlertType.kt
@@ -0,0 +1,5 @@
+package pl.edu.agh.dockerbuddy.model
+
+enum class AlertType {
+    OK, WARN, CRITICAL
+}
--- backend/src/main/kotlin/pl/edu/agh/dockerbuddy/model/metric/BasicMetric.kt
+++ backend/src/main/kotlin/pl/edu/agh/dockerbuddy/model/metric/BasicMetric.kt
@@ -1,7 +1,11 @@
 package pl.edu.agh.dockerbuddy.model.metric
 
+import pl.edu.agh.dockerbuddy.model.AlertType
+
 data class BasicMetric(
         val value: Double,
         val total: Double,
-        val percent: Double
+        val percent: Double,
+        val alertType: AlertType?,
+        val alert: Boolean?
 )
