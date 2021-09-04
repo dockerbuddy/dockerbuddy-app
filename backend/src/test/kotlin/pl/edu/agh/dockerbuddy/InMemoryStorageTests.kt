ba1ff9555e624b7f28832c38dea19290119c3caf
Krzysztof Nalepa <krzysztof.nalepa@appliscale.io>
2021-09-04 18:38:27 +0200

    Tests added

---
+++ backend/src/test/kotlin/pl/edu/agh/dockerbuddy/InMemoryStorageTests.kt
@@ -0,0 +1,94 @@
+package pl.edu.agh.dockerbuddy
+
+import org.apache.catalina.Host
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import pl.edu.agh.dockerbuddy.inmemory.InMemory
+import pl.edu.agh.dockerbuddy.inmemory.InMemoryStorage
+import pl.edu.agh.dockerbuddy.model.metric.HostSummary
+
+class InMemoryStorageTests {
+    @Test
+    fun saveHostSummaryTest1() {
+        //given
+        val inMemory: InMemory = InMemoryStorage()
+        val id: Long = 1
+        val hostSummary: HostSummary = Mockito.mock(HostSummary::class.java)
+
+        //when
+        inMemory.saveHostSummary(id, hostSummary)
+
+        //then
+        Assertions.assertEquals(hostSummary, inMemory.getHostSummary(id))
+    }
+
+    @Test
+    fun saveHostSummaryTest2() {
+        //given
+        val inMemory: InMemory = InMemoryStorage()
+        val id: Long = -1
+        val hostSummary: HostSummary = Mockito.mock(HostSummary::class.java)
+
+        //when
+        inMemory.saveHostSummary(id, hostSummary)
+
+        //then
+        Assertions.assertEquals(hostSummary, inMemory.getHostSummary(id))
+    }
+
+    @Test
+    fun getAllHostSummariesTest1() {
+        //given
+        val inMemory: InMemory = InMemoryStorage()
+        val hostSummary1: HostSummary = Mockito.mock(HostSummary::class.java)
+        val hostSummary2: HostSummary = Mockito.mock(HostSummary::class.java)
+
+        //when
+        inMemory.saveHostSummary(1, hostSummary1)
+        inMemory.saveHostSummary(2, hostSummary2)
+
+        //then
+        Assertions.assertEquals(listOf(hostSummary1, hostSummary2), inMemory.getAllHostSummaries())
+    }
+
+    @Test
+    fun getAllHostSummariesTest2() {
+        //given
+        val inMemory: InMemory = InMemoryStorage()
+
+        //then
+        Assertions.assertEquals(emptyList<HostSummary>(), inMemory.getAllHostSummaries())
+    }
+
+    @Test
+    fun deleteHostSummaryTest1() {
+        //given
+        val inMemory: InMemory = InMemoryStorage()
+        val id: Long = 1
+        val hostSummary: HostSummary = Mockito.mock(HostSummary::class.java)
+
+        //when
+        inMemory.saveHostSummary(id, hostSummary)
+        inMemory.deleteHost(id)
+
+        //then
+        Assertions.assertEquals(emptyList<HostSummary>(), inMemory.getAllHostSummaries())
+    }
+
+    @Test
+    fun deleteHostSummaryTest2() {
+        //given
+        val inMemory: InMemory = InMemoryStorage()
+        val id: Long = 1
+        val hostSummary: HostSummary = Mockito.mock(HostSummary::class.java)
+
+        //when
+        inMemory.saveHostSummary(id, hostSummary)
+        inMemory.deleteHost(id)
+
+        //then
+        Assertions.assertEquals(null, inMemory.getHostSummary(id))
+    }
+}
