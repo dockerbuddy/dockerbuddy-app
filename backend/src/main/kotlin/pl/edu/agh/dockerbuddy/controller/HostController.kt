136d062a6cb96b43521d7787818cbfd9b1127cab
Jakub Solecki <57220835+jakubsolecki@users.noreply.github.com>
2021-09-05 04:49:04 +0200

    added host info endpoint (#53)
    * added host info endpoint
    
    that returns a List of hosts with summary
    
    * url path refactor

--- backend/src/main/kotlin/pl/edu/agh/dockerbuddy/controller/HostController.kt
+++ backend/src/main/kotlin/pl/edu/agh/dockerbuddy/controller/HostController.kt
@@ -2,17 +2,14 @@ package pl.edu.agh.dockerbuddy.controller
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import pl.edu.agh.dockerbuddy.controller.response.DefaultResponse
 import pl.edu.agh.dockerbuddy.controller.response.ResponseType
 import pl.edu.agh.dockerbuddy.model.entity.Host
 import pl.edu.agh.dockerbuddy.service.HostService
 
 @RestController
-@RequestMapping("/api/v2/host")
+@RequestMapping("/api/v2/hosts")
 class HostController (
     val hostService: HostService
 ) {
@@ -23,6 +20,18 @@ class HostController (
             val savedHost = hostService.addHost(host)
             ResponseEntity.status(HttpStatus.CREATED)
                 .body(DefaultResponse(ResponseType.SUCCESS, "Host added", savedHost))
+        } catch (e: Exception){
+            ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
+                .body(DefaultResponse(ResponseType.ERROR, e.message.toString(), null))
+        }
+    }
+
+    @GetMapping
+    fun getHostsWithSummary(): ResponseEntity<DefaultResponse> {
+        return try {
+            val hostWithSummary = hostService.getHostsWithSummary()
+            ResponseEntity.status(HttpStatus.OK)
+                .body(DefaultResponse(ResponseType.SUCCESS, "Host added", hostWithSummary))
         } catch (e: Exception){
             ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
                 .body(DefaultResponse(ResponseType.ERROR, e.message.toString(), null))
