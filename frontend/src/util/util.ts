1af5c8962906f53bc520201995b522ba3c97dcc9
Krzysztof Nalepa <krzysztof.nalepa@appliscale.io>
2021-09-18 20:46:12 +0200

    undefiend removed from disk usage

--- frontend/src/util/util.ts
+++ frontend/src/util/util.ts
@@ -7,5 +7,9 @@ export function humanFileSize(size: number): string {
   if (size === 0) return "0";
   const i = Math.floor(Math.log(+size) / Math.log(1024));
   const y: any = +size / Math.pow(1024, i); //TODO number throw error
-  return y.toFixed(2) * 1 + " " + ["B", "kB", "MB", "GB", "TB"][i];
+  return (
+    y.toFixed(2) * 1 +
+    " " +
+    ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"][i]
+  );
 }
