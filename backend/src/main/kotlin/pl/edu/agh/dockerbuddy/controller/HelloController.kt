f411e8e23445bbd0f2919f4d61b5c87b74cd5197
Jakub Solecki <jakub.solecki99@gmail.com>
2021-09-18 16:40:13 +0200

    added CrossOrigin annotations on endpoints

--- backend/src/main/kotlin/pl/edu/agh/dockerbuddy/controller/HelloController.kt
+++ backend/src/main/kotlin/pl/edu/agh/dockerbuddy/controller/HelloController.kt
@@ -1,9 +1,11 @@
 package pl.edu.agh.dockerbuddy.controller
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin
 @RestController
 @RequestMapping("/hello")
 class HelloController {
