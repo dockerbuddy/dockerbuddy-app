fea9d1071f8e420f96a18351ce07b902aa1ddd7b
Krzysztof Nalepa <krzysztof.nalepa@appliscale.io>
2021-10-19 16:30:35 +0200

    script modified and stuff

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ cd ./backend
 mvn clean
 mvn -DskipTests package
 cd ..
-docker build . -t dockerbuddy
+docker build . -t kraleppa/dockerbuddy:${1:-latest}
+cp ./backend/target/dockerbuddy-0.0.1-SNAPSHOT.jar ./dockerbuddy-${1:-latest}.jar
