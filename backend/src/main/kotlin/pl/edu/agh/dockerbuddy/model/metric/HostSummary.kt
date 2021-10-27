4c5e5b6f4cbc7b0ecb9806b232a33507efc7ace8
Jakub Solecki <jakub.solecki99@gmail.com>
2021-10-27 23:11:23 +0200

    timestamp validation for HostSummary

--- backend/src/main/kotlin/pl/edu/agh/dockerbuddy/model/metric/HostSummary.kt
+++ backend/src/main/kotlin/pl/edu/agh/dockerbuddy/model/metric/HostSummary.kt
@@ -1,10 +1,12 @@
 package pl.edu.agh.dockerbuddy.model.metric
 
 import lombok.ToString
+import javax.validation.constraints.Pattern
 
 @ToString
 data class HostSummary(
     val id: Long,
+    @field:Pattern(regexp = "^[1-9]\\d{3}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z\$")
     val timestamp: String,
     val metrics: List<BasicMetric>,
     val containers: List<ContainerSummary>
