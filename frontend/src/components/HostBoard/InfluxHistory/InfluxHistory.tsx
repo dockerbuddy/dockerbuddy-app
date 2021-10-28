e022c7513d7dc19aafe39038e504d3780092bea0
Patryk Skupien <pskupien@akamai.com>
2021-10-29 00:49:04 +0200

    metric history fix part 1

--- frontend/src/components/HostBoard/InfluxHistory/InfluxHistory.tsx
+++ frontend/src/components/HostBoard/InfluxHistory/InfluxHistory.tsx
@@ -1,5 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable */
 import { Button, Grid, Select, TextField } from "@material-ui/core";
+import { DatePicker } from "@mui/lab";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { fetchHostHistory, QueryParams } from "../../../common/api";
@@ -46,78 +47,96 @@ const InfluxHistory: React.FC<{ hostId: number }> = (props) => {
     }
   };
 
+  const [start, setStart] = React.useState(new Date());
+
+  const tmp = (newValue: Date | null) => {
+    if (newValue !== null)
+      setStart(newValue);
+  }
+
   return (
-    <Grid container item spacing={4}>
-      <Grid item xs={2}>
-        <form onSubmit={handleSubmit(handleQuery)}>
-          <Grid container item direction="column" spacing={4}>
-            <Grid item>
-              <Select
-                native
-                defaultValue="Metric type"
-                name="metricType"
-                id="metric-type-select"
-                label="Metric"
-                inputRef={register({
-                  required: "Required",
-                })}
-              >
-                <option value="">Metric-type</option>
-                {Object.keys(tmp).map((key: string) => {
-                  return (
-                    <optgroup key={key} label={key}>
-                      {tmp[key as keyof tmpp].map((val: string) => {
-                        return (
-                          <option key={val} value={val}>
-                            {val}
-                          </option>
-                        );
-                      })}
-                    </optgroup>
-                  );
-                })}
-              </Select>
-            </Grid>
-            <Grid item>
-              <TextField
-                name="start"
-                label="Start time"
-                inputRef={register({
-                  required: "Start time is required",
-                })}
-                error={!!errors.start}
-                helperText={errors.start?.message}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                name="end"
-                label="End time"
-                inputRef={register({
-                  required: "End time is required",
-                })}
-                error={!!errors.start}
-                helperText={errors.start?.message}
-              />
-            </Grid>
-            <Grid item>
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                disableElevation
-                fullWidth
-              >
-                Query
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Grid>
-      <Grid item xs={10}>
-        {typeof hostHistory !== "undefined" && <MainChart body={hostHistory} />}
+    <Grid container spacing={4}>
+      <Grid item>
+        {/* <DatePicker 
+          label="Start"
+          inputFormat="dd/MM/yyyy"
+          value={start}
+          onChange={tmp}
+          renderInput={(params) => <TextField {...params} />}
+          /> */}
       </Grid>
     </Grid>
+    // <Grid container item spacing={4}>
+    //   <Grid item xs={2}>
+    //     <form onSubmit={handleSubmit(handleQuery)}>
+    //       <Grid container item direction="column" spacing={4}>
+    //         <Grid item>
+    //           <Select
+    //             native
+    //             defaultValue="Metric type"
+    //             name="metricType"
+    //             id="metric-type-select"
+    //             label="Metric"
+    //             inputRef={register({
+    //               required: "Required",
+    //             })}
+    //           >
+    //             <option value="">Metric-type</option>
+    //             {Object.keys(tmp).map((key: string) => {
+    //               return (
+    //                 <optgroup key={key} label={key}>
+    //                   {tmp[key as keyof tmpp].map((val: string) => {
+    //                     return (
+    //                       <option key={val} value={val}>
+    //                         {val}
+    //                       </option>
+    //                     );
+    //                   })}
+    //                 </optgroup>
+    //               );
+    //             })}
+    //           </Select>
+    //         </Grid>
+    //         <Grid item>
+    //           <TextField
+    //             name="start"
+    //             label="Start time"
+    //             inputRef={register({
+    //               required: "Start time is required",
+    //             })}
+    //             error={!!errors.start}
+    //             helperText={errors.start?.message}
+    //           />
+    //         </Grid>
+    //         <Grid item>
+    //           <TextField
+    //             name="end"
+    //             label="End time"
+    //             inputRef={register({
+    //               required: "End time is required",
+    //             })}
+    //             error={!!errors.start}
+    //             helperText={errors.start?.message}
+    //           />
+    //         </Grid>
+    //         <Grid item>
+    //           <Button
+    //             type="submit"
+    //             variant="contained"
+    //             color="primary"
+    //             disableElevation
+    //             fullWidth
+    //           >
+    //             Query
+    //           </Button>
+    //         </Grid>
+    //       </Grid>
+    //     </form>
+    //   </Grid>
+    //   <Grid item xs={10}>
+    //     {typeof hostHistory !== "undefined" && <MainChart body={hostHistory} />}
+    //   </Grid>
+    // </Grid>
   );
 };
 
