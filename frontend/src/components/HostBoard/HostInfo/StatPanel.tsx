5ebe9c085c864c2d8353bac7d04d69374e420a3d
Krzysztof Nalepa <krzysztof.nalepa@appliscale.io>
2021-11-24 01:00:54 +0100

    stat panel update

--- frontend/src/components/HostBoard/HostInfo/StatPanel.tsx
+++ frontend/src/components/HostBoard/HostInfo/StatPanel.tsx
@@ -2,7 +2,8 @@ import { Grid, Typography } from "@material-ui/core";
 import React from "react";
 import { MetricType } from "../../../common/enums";
 import { Host } from "../../../common/types";
-import { extractMetricPercent } from "../../../util/util";
+import { extractMetricBasic, extractMetricPercent } from "../../../util/util";
+import NetworkInfo from "../../Dashboard/NetworkInfo";
 import ProgressBarComponent from "../../Dashboard/ProgressBarComponent";
 
 interface StatPanelProps {
@@ -10,10 +11,30 @@ interface StatPanelProps {
 }
 
 const StatPanel: React.FC<StatPanelProps> = ({ hostData }) => {
-  const metric = hostData?.hostSummary?.percentMetrics;
-  const cpu_metric = extractMetricPercent(metric, MetricType.CPU_USAGE);
-  const memory_metric = extractMetricPercent(metric, MetricType.MEMORY_USAGE);
-  const disk_metric = extractMetricPercent(metric, MetricType.DISK_USAGE);
+  const hostSummary = hostData?.hostSummary;
+
+  const diskUsage = extractMetricPercent(
+    hostSummary?.percentMetrics,
+    MetricType.DISK_USAGE
+  );
+  const memoryUsage = extractMetricPercent(
+    hostSummary?.percentMetrics,
+    MetricType.MEMORY_USAGE
+  );
+  const cpuUsage = extractMetricPercent(
+    hostSummary?.percentMetrics,
+    MetricType.CPU_USAGE
+  );
+
+  const networkOut = extractMetricBasic(
+    hostSummary?.basicMetrics,
+    MetricType.NETWORK_OUT
+  );
+
+  const networkIn = extractMetricBasic(
+    hostSummary?.basicMetrics,
+    MetricType.NETWORK_IN
+  );
 
   return (
     <Grid container direction="column" spacing={2}>
@@ -21,13 +42,16 @@ const StatPanel: React.FC<StatPanelProps> = ({ hostData }) => {
         <Typography variant="h5">Current status</Typography>
       </Grid>
       <Grid item>
-        <ProgressBarComponent name={"Disk"} metric={disk_metric} />
+        <ProgressBarComponent name={"Disk"} metric={diskUsage} />
       </Grid>
       <Grid item>
-        <ProgressBarComponent name={"Memory"} metric={memory_metric} />
+        <ProgressBarComponent name={"Memory"} metric={memoryUsage} />
       </Grid>
       <Grid item>
-        <ProgressBarComponent name={"CPU"} metric={cpu_metric} />
+        <ProgressBarComponent name={"CPU"} metric={cpuUsage} />
+      </Grid>
+      <Grid item>
+        <NetworkInfo networkIn={networkIn} networkOut={networkOut} />
       </Grid>
     </Grid>
   );
